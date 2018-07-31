a9f16a5b998255b08a5429c71b2c578761e8e598
richjimdev <richjimdev@gmail.com>
2018-07-31 15:13:28 -0700

    Added simple CSS and started on javascript file

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-console.log("js is linked");
+"use strict";
+
+console.log('js is linked');
+
+var yourName = prompt('Welcome to my website! Before we begin let\'s play a quick game. First, tell me your name:');
+console.log('User name is ' + yourName + '.');
